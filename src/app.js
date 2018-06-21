35d486e7a19bb08ccf92630ee37824160b68d5be
LeelooE <leeloonita@gmail.com>
2018-06-20 18:42:55 -0600

    Object Created

--- src/app.js
+++ src/app.js
@@ -2,7 +2,32 @@ console.log("App.js is running!");
 
 //JSX - JavaScript XML
 
-var template = <h1>Indecision App</h1>;
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life into the hands of the computer!"
+}
+var template = (
+    <div>
+        <h1>{app.title}</h1> 
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
+var user = {
+    name: 'Anita',
+    age: 18,
+    location: 'Spokane'
+}
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
