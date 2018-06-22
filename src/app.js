8437f70e842cc12741246d476d7f641913b29034
LeelooE <leeloonita@gmail.com>
2018-06-21 22:29:21 -0600

    ES6 const and let playground added

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ console.log("App.js is running!");
 
 //JSX - JavaScript XML
 
-var app = {
+const app = {
     title: "Indecision App",
     subtitle: "Put your life into the hands of the computer!",
     options: ['One', 'Two']
 }
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1> 
         {app.subtitle && <p>{app.subtitle}</p>}
@@ -18,7 +18,7 @@ var template = (
         </ol>
     </div>
 );
-var user = {
+const user = {
     name: 'Anita',
     age: 18,
     location: 'Spokane'
@@ -28,13 +28,13 @@ function getLocation(location){
         return <p>Location: {location}</p>;
     }
 };
-var templateTwo = (
+const templateTwo = (
     <div>
         <h1>{user.name ? user.name : 'Anonymous'}</h1>
         {(user.age && user.age >=18) && <p>Age: {user.age}</p>}
         {getLocation(user.location)}
     </div>
 );
-var appRoot = document.getElementById("app");
+const appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
