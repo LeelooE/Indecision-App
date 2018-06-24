b6782fe53c35d3d4ce9433d0a5d83ed234c878e7
LeelooE <leeloonita@gmail.com>
2018-06-23 21:14:53 -0600

    Subclasses added to ES6-classes-1.js classes

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -9,12 +9,47 @@ class Person {
         return `Hi. I am ${ this.name }!`
     }
     getDescription(){
-        return `${ this.name } is ${ this.age } year(s) old`
+        return `${ this.name } is ${ this.age } year(s) old.`
     }
 }
 
-const me = new Person('Anita Silva', 18);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor(){
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        
+        if (this.hasMajor()) {
+            description += ` Their major is ${ this.major }`;
+        }
 
-const other = new Person();
-console.log(other.getDescription());
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, major, homeLocation){
+        super(name, age, major);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting(){
+        let greeting = super.getGreeting();
+
+        if(this.homeLocation) {
+            greeting += ` I'm visiting from ${ this.homeLocation }.`;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Traveler('Anita Silva', 18, 'Computer Science', 'Spokane');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
