b83490f5cb8c6f3ebfd9a6ce6d7be335184a2959
LeelooE <leeloonita@gmail.com>
2018-06-26 21:42:01 -0600

    Modules react and react-dom added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-//import './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import subtract, {square, add} from './utils.js';
-
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(6, 4));
-// console.log(subtract(5, 5));
-
-import  isSenior, {canDrink, isAdult,} from './person.js';
-console.log(canDrink(18));
-console.log(isAdult(18));
-console.log(isSenior(65));
+const template = React.createElement ('p', {}, 'test');
+ReactDOM.render(template, document.getElementById('app'));
