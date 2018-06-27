39a2404de56c49ea7056797c68ac5243348e1ca9
LeelooE <leeloonita@gmail.com>
2018-06-26 19:52:42 -0600

    added default exports

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 //import './utils.js';
 
-// import {square, add} from './utils.js';
+// import subtract, {square, add} from './utils.js';
 
 // console.log('app.js is running');
 // console.log(square(4));
 // console.log(add(6, 4));
+// console.log(subtract(5, 5));
 
-import  {canDrink, isAdult} from './person.js';
+import  isSenior, {canDrink, isAdult,} from './person.js';
 console.log(canDrink(18));
 console.log(isAdult(18));
+console.log(isSenior(65));
--- src/utils.js
+++ src/utils.js
@@ -5,4 +5,6 @@ export const square = (x) => x * x;
 
 export const add = (a, b) => a + b;
 
-//export {square, add};
+export default (a, b) => a - b;
+
+//export {square, add, subtract as default};
