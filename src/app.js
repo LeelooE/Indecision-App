d4d8615a00eda2d6c2cd489041a2746aabda4b46
LeelooE <leeloonita@gmail.com>
2018-06-26 19:32:38 -0600

    added export and imports

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('app.js is running');
+//import './utils.js';
+
+// import {square, add} from './utils.js';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(6, 4));
+
+import  {canDrink, isAdult} from './person.js';
+console.log(canDrink(18));
+console.log(isAdult(18));
